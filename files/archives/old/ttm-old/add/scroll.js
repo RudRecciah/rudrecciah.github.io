24c4b0a07881bf19113415b26ac31c7bbaaabaa5
RudRecciah <me@rudrecciah.dev>
2021-04-23 16:17:48 -0400

    edited some archive files

--- files/archives/old/ttm-old/add/scroll.js
+++ files/archives/old/ttm-old/add/scroll.js
@@ -1,17 +1,17 @@
-const doc = document;
-const navbar = doc.querySelector(".navbar-container");
-    limit = 300  /* scrolltop value when opacity should be 0 */
-
-function(window).on('scroll', function() {
-   var st = $(this).scrollTop();
-
-   /* avoid unnecessary call to jQuery function */
-   if (st <= limit) {
-    navbar.classList.add("navbar-container-opacity");
-    console.log("test");
-   }
-   else {
-    navbar.classList.remove("navbar-container-opacity");
-    console.log("test2");
-   }
-});
+// const doc = document;
+// const navbar = doc.querySelector(".navbar-container");
+//     limit = 300  /* scrolltop value when opacity should be 0 */
+//
+// function(window).on('scroll', function() {
+//    var st = $(this).scrollTop();
+//
+//    /* avoid unnecessary call to jQuery function */
+//    if (st <= limit) {
+//     navbar.classList.add("navbar-container-opacity");
+//     console.log("test");
+//    }
+//    else {
+//     navbar.classList.remove("navbar-container-opacity");
+//     console.log("test2");
+//    }
+// });
