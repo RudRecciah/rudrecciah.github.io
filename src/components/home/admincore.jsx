76c333604f379dd80fd8337d4512b5aa7fe939a3
RudRecciah <53617174+RudRecciah@users.noreply.github.com>
2021-06-18 13:25:48 -0400

    finished admincore section except for mobile features boxes

--- src/components/home/admincore.jsx
+++ src/components/home/admincore.jsx
@@ -1,14 +1,12 @@
 import React, {Component} from "react";
 import {Col, Container, Row} from "reactstrap";
-import langs from "../../assets/data/langs.json";
-import AdmincoreFeatures from "./admincoreFeatures.jsx";
+import SpacerSm from "../utils/spacerSm.jsx";
+import SpacerMd from "../utils/spacerMd.jsx";
 
 export default class Admincore extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      langs: langs.names,
-    };
+    this.state = {};
   }
   
   render() {
@@ -31,14 +29,148 @@ export default class Admincore extends Component {
                   </Row>
                   <Row className="d-none d-lg-flex flex-row">
                     <Col>
-                      <AdmincoreFeatures/>
+                      <div className="box">
+                        <div className="border-bottom-box box-top">
+                          <span className="h-100 d-flex flex-column justify-content-center align-content-center w-100 text-center c-text-gray">
+                            <span className="text-right mr-3">
+                              Report Player
+                            </span>
+                          </span>
+                        </div>
+                        <div className="d-flex flex-row">
+                          <div className="fake-content fake-content-row fake-content-last text-gray text-center w-25">&#8197;</div>
+                          <div className="fake-content fake-content-last text-gray text-center w-75">&#8197;</div>
+                        </div>
+                      </div>
+                      <SpacerSm/>
+                      <div className="box">
+                        <div className="border-bottom-box box-top">
+                          <span className="h-100 d-flex flex-column justify-content-center align-content-center w-100 text-center c-text-gray">
+                            <span className="text-right mr-3">
+                              Ban Player
+                            </span>
+                          </span>
+                        </div>
+                        <div className="d-flex flex-row">
+                          <div className="fake-content fake-content-row fake-content-last text-gray text-center w-75">&#8197;</div>
+                          <div className="fake-content fake-content-row fake-content-last text-gray text-center w-13-5">&#8197;</div>
+                          <div className="fake-content fake-content-last text-gray text-center w-13-5">&#8197;</div>
+                        </div>
+                      </div>
+                      <SpacerSm/>
+                      <div className="box">
+                        <div className="border-bottom-box box-top">
+                          <span className="h-100 d-flex flex-column justify-content-center align-content-center w-100 text-center c-text-gray">
+                            <span className="text-right mr-3">
+                              RudRecciah's Statistics
+                            </span>
+                          </span>
+                        </div>
+                        <div className="d-flex flex-row">
+                          <div className="fake-content fake-content-row fake-content-last text-gray text-center w-20">&#8197;</div>
+                          <div className="fake-content fake-content-row fake-content-last text-gray text-center w-20">&#8197;</div>
+                          <div className="fake-content fake-content-row fake-content-last text-gray text-center w-20">&#8197;</div>
+                          <div className="fake-content fake-content-row fake-content-last text-gray text-center w-20">&#8197;</div>
+                          <div className="fake-content fake-content-last text-gray text-center w-20">&#8197;</div>
+                        </div>
+                      </div>
+                      <SpacerSm/>
+                      <div className="box">
+                        <div className="border-bottom-box box-top">
+                          <span className="h-100 d-flex flex-column justify-content-center align-content-center w-100 text-center c-text-gray">
+                            <span className="text-right mr-3">
+                              . . .
+                            </span>
+                          </span>
+                        </div>
+                        <div className="d-flex flex-row">
+                          <div className="fake-content fake-content-row fake-content-last text-gray text-center w-50">&#8197;</div>
+                          <div className="fake-content fake-content-last text-gray text-center w-50">&#8197;</div>
+                        </div>
+                      </div>
                     </Col>
                   </Row>
                 </Col>
                 <Col xs="12" lg="6">
-                  <h3 className="font-header text-center text-white">Customization</h3>
-                  <p className="text-center font-size-p c-text-gray">While Admincore requires minimal setup and can operate as a nearly drag-and-drop solution, Admincore is also built with customization in mind. Admincore comes with over 150 configuration options, allowing operators to make it fit their specific use cases.</p>
+                  <Row>
+                    <Col>
+                      <h3 className="font-header text-center text-white">Customization</h3>
+                      <p className="text-center font-size-p c-text-gray d-1070-pre">While Admincore requires minimal setup and can operate as a nearly drag-and-drop solution, Admincore is also built with customization in mind. Admincore comes with over 150 configuration options, allowing operators to make it fit their specific use cases.</p>
+                      <p className="text-center font-size-p c-text-gray d-1070-post">While Admincore can operate as a nearly drag-and-drop solution, Admincore is also built with customization in mind. Admincore comes with over 150 configuration options, allowing operators to make it fit their specific use cases.</p>
+                    </Col>
+                  </Row>
+                  <Row className="d-none d-lg-flex flex-row">
+                    <Col>
+                      <div className="box">
+                        <div className="border-bottom-box box-top">
+                          <span className="h-100 d-flex flex-column justify-content-center align-content-center w-100 text-center c-text-gray">
+                            <span className="text-left ml-3">
+                              Discord Integration
+                            </span>
+                          </span>
+                        </div>
+                        <div className="d-flex flex-row">
+                          <div className="fake-content fake-content-row fake-content-last text-gray text-center w-25">&#8197;</div>
+                          <div className="fake-content fake-content-row fake-content-last text-gray text-center w-61-5">&#8197;</div>
+                          <div className="fake-content fake-content-last text-gray text-center w-13-5">&#8197;</div>
+                        </div>
+                      </div>
+                      <SpacerSm/>
+                      <div className="box">
+                        <div className="border-bottom-box box-top">
+                          <span className="h-100 d-flex flex-column justify-content-center align-content-center w-100 text-center c-text-gray">
+                            <span className="text-left ml-3">
+                              Staffmode
+                            </span>
+                          </span>
+                        </div>
+                        <div className="d-flex flex-row">
+                          <div className="fake-content fake-content-row fake-content-last text-gray text-center w-20">&#8197;</div>
+                          <div className="fake-content fake-content-row fake-content-last text-gray text-center w-20">&#8197;</div>
+                          <div className="fake-content fake-content-row fake-content-last text-gray text-center w-20">&#8197;</div>
+                          <div className="fake-content fake-content-row fake-content-last text-gray text-center w-20">&#8197;</div>
+                          <div className="fake-content fake-content-last text-gray text-center w-20">&#8197;</div>
+                        </div>
+                      </div>
+                      <SpacerSm/>
+                      <div className="box">
+                        <div className="border-bottom-box box-top">
+                          <span className="h-100 d-flex flex-column justify-content-center align-content-center w-100 text-center c-text-gray">
+                            <span className="text-left ml-3">
+                              Punishment Length
+                            </span>
+                          </span>
+                        </div>
+                        <div className="d-flex flex-row">
+                          <div className="fake-content fake-content-row fake-content-last text-gray text-center w-25">&#8197;</div>
+                          <div className="fake-content fake-content-last text-gray text-center w-75">&#8197;</div>
+                        </div>
+                      </div>
+                      <SpacerSm/>
+                      <div className="box">
+                        <div className="border-bottom-box box-top">
+                          <span className="h-100 d-flex flex-column justify-content-center align-content-center w-100 text-center c-text-gray">
+                            <span className="text-left ml-3">
+                              . . .
+                            </span>
+                          </span>
+                        </div>
+                        <div className="d-flex flex-row">
+                          <div className="fake-content fake-content-row fake-content-last text-gray text-center w-50">&#8197;</div>
+                          <div className="fake-content fake-content-last text-gray text-center w-50">&#8197;</div>
+                        </div>
+                      </div>
+                    </Col>
+                  </Row>
                 </Col>
+                <SpacerMd/>
+              </Row>
+              <Row>
+                <Col/>
+                <Col className="text-center">
+                  <a href="./admincore" className="btn btn-white text-center">Learn More</a>
+                </Col>
+                <Col/>
               </Row>
             </section>
           </Container>
