26525225b393a30bf67e2df81b2c459641aa2a80
RudRecciah <me@rudrecciah.dev>
2021-07-03 01:07:09 -0400

    dear god im choosing different themes more than a teenage girl has mood swings

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,4 +1,7 @@
 import React, {Component} from "react";
+import {Anchor, Box, Footer, Header, Nav, Text} from "grommet";
+import {Code} from "grommet-icons";
+import {Link as BrowserLink} from "react-router-dom";
 
 export default class Home extends Component {
   constructor(props) {
@@ -9,7 +12,22 @@ export default class Home extends Component {
   render() {
     return (
       <React.Fragment>
-
+        <Box direction={"column"} className={"h-100vh"}>
+          <Header background={"brand"} pad={"medium"}>
+            <Nav margin={{"horizontal": "xlarge"}} direction={"row"}>
+              <BrowserLink to={"/"}>
+                <Anchor>
+                  <Code color={"accent-1"}/>
+                </Anchor>
+              </BrowserLink>
+            </Nav>
+          </Header>
+          <Box fill={true} margin={"xlarge"}>
+            <h1 className={"font-overpass"}>Hey</h1>
+            <Text>I'm a pink parrot who likes code. I've been developing for the past three years, working on various projects mostly in Java.</Text>
+          </Box>
+          <Footer/>
+        </Box>
       </React.Fragment>
     );
   }
